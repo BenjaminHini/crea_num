c1ba60ad51a7d21764f3990926f1af56741d052a
DanyChx <57039857+DanyChx@users.noreply.github.com>
2021-12-04 16:16:17 +0100

    network

---
+++ Menu_Acceuil.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_heberger_pressed():
+	pass # Replace with function body.
+
+
+func _on_rejoindre_pressed():
+	pass # Replace with function body.
