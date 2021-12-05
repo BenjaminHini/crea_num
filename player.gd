416664f0105c91bf6c11ae9d108d8b032f1cfdb4
BenjaminHini <benjamin.hini@edu.ece.fr>
2021-12-05 18:54:32 +0100

    pofinable + manque animations

--- player.gd
+++ player.gd
@@ -2,16 +2,21 @@ extends KinematicBody2D
 
 
 const UP = Vector2(0,-1)
-const JUMP = 50
+const JUMP = 400
 const GRAVITY = 20
-const MAXSPEED = 50
+const MAXSPEED = 150
 const ACCEL = 10
 
 var movement = Vector2()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+<<<<<<< Updated upstream
 	movement.y += GRAVITY
 	
+=======
+	
+	movement.y += GRAVITY
+>>>>>>> Stashed changes
 	movement.x = clamp(movement.x, -MAXSPEED, MAXSPEED)
 
 	if Input.is_action_pressed("right"):
@@ -21,8 +26,16 @@ func _physics_process(delta):
 	else: 
 		movement.x = lerp(movement.x, 0, 0.2)
 	
+<<<<<<< Updated upstream
 	if is_on_floor():
 		if Input.is_action_pressed("jump"):
 			movement.y = -JUMP
 			
 	#motion = move_and_slide(motion, UP)
+=======
+	#if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			movement.y = -JUMP
+			
+	move_and_slide(movement)
+>>>>>>> Stashed changes
