f9780990a57ded0ef1393a3b15d24a9b7ad24137
BenjaminHini <benjamin.hini@edu.ece.fr>
2021-12-05 17:53:00 +0100

    ca marche aps

--- player.gd
+++ player.gd
@@ -1,33 +1,28 @@
 extends KinematicBody2D
 
+
 const UP = Vector2(0,-1)
 const JUMP = 50
 const GRAVITY = 20
 const MAXSPEED = 50
 const ACCEL = 10
 
-
-
-var movemement = Vector2()
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-func _physics(delta):
+var movement = Vector2()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	movement.y += GRAVITY
 	
-	movemement.y += GRAVITY
-	
-	movemement.x = clamp(movemement.x, -MAXSPEED, MAXSPEED)
-	
+	movement.x = clamp(movement.x, -MAXSPEED, MAXSPEED)
+
 	if Input.is_action_pressed("right"):
-		movemement.x = MAXSPEED
+		movement.x = MAXSPEED
 	elif Input.is_action_pressed("left"):
-		movemement.x = -MAXSPEED
-	else:
-		movemement.x = lerp(movemement.x, 0, 0.2)
-		
+		movement.x = -MAXSPEED
+	else: 
+		movement.x = lerp(movement.x, 0, 0.2)
+	
 	if is_on_floor():
-		if Input.is_action_pressed("space"):
-			movemement.y = -JUMP
-
-	movemement = move_and_slide(movemement, UP)
+		if Input.is_action_pressed("jump"):
+			movement.y = -JUMP
+			
+	#motion = move_and_slide(motion, UP)
